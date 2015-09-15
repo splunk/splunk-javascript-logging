1a96a7733e30f578ea2030947070754fa86822e7
Shakeel Mohamed <smohamed@splunk.com>
2015-09-14 17:17:18 -0700

    remove comments

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var SplunkLogger = require("./splunklogger");
 var utils = require("./utils");
 
-// TODO: exports tag docs
 module.exports = {
-    Logger: SplunkLogger, // TODO: move SplunkLogger its own file
+    Logger: SplunkLogger,
     utils: utils
 };
